d4fa7e98f6fd24948870d8e263adc41c61caa4f1
or-else <or.else@gmail.com>
2017-09-08 16:39:30 -0700

    another crash fixed

--- app/src/main/java/co/tinode/tindroid/media/SpanFormatter.java
+++ app/src/main/java/co/tinode/tindroid/media/SpanFormatter.java
@@ -3,6 +3,7 @@ package co.tinode.tindroid.media;
 import android.graphics.Typeface;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.text.SpannedString;
 import android.text.style.CharacterStyle;
 import android.text.style.StrikethroughSpan;
 import android.text.style.StyleSpan;
@@ -21,6 +22,11 @@ import co.tinode.tinodesdk.model.Drafty;
 public class SpanFormatter {
 
     public static Spanned toSpanned(Drafty content) {
+        if (content == null) {
+            // Malicious user may send a message with null content.
+            return new SpannedString("");
+        }
+
         SpannableStringBuilder text = new SpannableStringBuilder(content.toString());
         Drafty.Style[] fmt = content.getStyles();
 
