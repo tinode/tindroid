59398daef105b9d729e6512d4bf569a7ef904704
or-else <or.else@gmail.com>
2019-11-04 18:49:41 +0300

    added basic pinch to zoon to image preview

--- app/src/main/java/co/tinode/tindroid/ImageViewFragment.java
+++ app/src/main/java/co/tinode/tindroid/ImageViewFragment.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -21,11 +23,23 @@ import android.widget.TextView;
  */
 
 public class ImageViewFragment extends Fragment {
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+    private ImageView mImageView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_image, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_image, container, false);
+        mImageView = view.findViewById(R.id.image);
+        mScaleGestureDetector = new ScaleGestureDetector(getActivity(), new ScaleListener());
+        view.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return mScaleGestureDetector.onTouchEvent(event);
+            }
+        });
+
+        return view;
     }
 
 
@@ -48,6 +62,17 @@ public class ImageViewFragment extends Fragment {
             ((TextView) activity.findViewById(R.id.content_type)).setText(args.getString("mime"));
             ((TextView) activity.findViewById(R.id.file_name)).setText(args.getString("name"));
             ((TextView) activity.findViewById(R.id.image_size)).setText(size + UiUtils.bytesToHumanSize(bits.length));
+        }
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.3f, Math.min(mScaleFactor, 10.0f));
+            mImageView.setScaleX(mScaleFactor);
+            mImageView.setScaleY(mScaleFactor);
+            return true;
         }
     }
 }
