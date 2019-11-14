2941a409f4f704bfce6486c517e9e729823e82c2
or-else <or.else@gmail.com>
2019-11-14 11:03:24 +0300

    correct flags hen launching from push notification

--- app/src/main/java/co/tinode/tindroid/fcm/FBaseMessagingService.java
+++ app/src/main/java/co/tinode/tindroid/fcm/FBaseMessagingService.java
@@ -150,8 +150,8 @@ public class FBaseMessagingService extends FirebaseMessagingService {
             intent.putExtra("topic", topic);
         }
 
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
