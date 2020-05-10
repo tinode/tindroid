a1e6f025696d1202c277f4202deb3553f457dc5f
or-else <or.else@gmail.com>
2020-05-10 11:09:35 +0300

    attempt to work around material-components bug

--- app/src/main/java/co/tinode/tindroid/LoginActivity.java
+++ app/src/main/java/co/tinode/tindroid/LoginActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
@@ -62,6 +63,11 @@ public class LoginActivity extends AppCompatActivity {
 
     private AccountAuthenticatorResponse mAccountAuthenticatorResponse = null;
     private Bundle mResultBundle = null;
+
+    static {
+        // Otherwise crash on API 21.
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
