baa87a8b1abb4e11948806e9b70b0fe14dbdaa0f
Vadim Uzbekov <eldarovich99@mail.ru>
2022-02-03 12:58:51 +0300

    Fixed PrivateType parsing

--- tinodesdk/src/main/java/co/tinode/tinodesdk/model/PrivateType.java
+++ tinodesdk/src/main/java/co/tinode/tinodesdk/model/PrivateType.java
@@ -17,6 +17,16 @@ public class PrivateType extends HashMap<String,Object> implements Mergeable, Se
         super();
     }
 
+    public PrivateType(String text) {
+        super();
+        String[] splitted = text.split(":");
+        int counter = 0;
+        while (splitted.length % 2 == 0 && counter < splitted.length) {
+            put(splitted[counter], splitted[counter + 1]);
+            counter = counter + 2;
+        }
+    }
+
     @JsonIgnore
     private Object getValue(String name) {
         Object value = get(name);
