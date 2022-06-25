69ffa20578234be31813edde6df0331ef68d1720
or-else <or.else@gmail.com>
2022-06-25 10:57:04 -0700

    crash when the fragment is closed too quickly

--- app/src/main/java/co/tinode/tindroid/MessagesFragment.java
+++ app/src/main/java/co/tinode/tindroid/MessagesFragment.java
@@ -1006,8 +1006,9 @@ public class MessagesFragment extends Fragment {
         }
 
         if (mAudioRecorder != null) {
-            mAudioRecorder.stop();
-            mAudioRecorder.reset();
+            try {
+                mAudioRecorder.stop();
+            } catch (RuntimeException ignored) {}
             mAudioRecorder.release();
             mAudioRecorder = null;
         }
