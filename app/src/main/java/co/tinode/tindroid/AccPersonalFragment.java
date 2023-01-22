8252845b859a876b4f8375a167d3c20baa5bf1c3
or-else <or.else@gmail.com>
2023-01-22 12:08:03 -0800

    clean up of unused resources

--- app/src/main/java/co/tinode/tindroid/AccPersonalFragment.java
+++ app/src/main/java/co/tinode/tindroid/AccPersonalFragment.java
@@ -10,10 +10,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.flexbox.FlexboxLayout;
