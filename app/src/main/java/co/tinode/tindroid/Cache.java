5b40c5273505585ca8d2b8b37d416d7414ad2c81
aforge <aforge@>
2023-02-01 17:52:32 -0800

    Always add video call listener to Tindroid.

--- app/src/main/java/co/tinode/tindroid/Cache.java
+++ app/src/main/java/co/tinode/tindroid/Cache.java
@@ -1,5 +1,6 @@
 package co.tinode.tindroid;
 
+import android.content.Intent;
 import android.os.Build;
 
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -8,6 +9,8 @@ import java.util.Locale;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 import co.tinode.tindroid.db.BaseDb;
 import co.tinode.tindroid.media.VxCard;
 import co.tinode.tindroid.services.CallConnection;
@@ -15,7 +18,10 @@ import co.tinode.tinodesdk.ComTopic;
 import co.tinode.tinodesdk.FndTopic;
 import co.tinode.tinodesdk.MeTopic;
 import co.tinode.tinodesdk.PromisedReply;
+import co.tinode.tinodesdk.Storage;
 import co.tinode.tinodesdk.Tinode;
+import co.tinode.tinodesdk.model.MsgServerData;
+import co.tinode.tinodesdk.model.MsgServerInfo;
 import co.tinode.tinodesdk.model.PrivateType;
 import co.tinode.tinodesdk.model.ServerMessage;
 
@@ -48,6 +54,69 @@ public class Cache {
 
             // Set device language
             sInstance.mTinode.setLanguage(Locale.getDefault().toString());
+
+            // Event handlers for video calls.
+            sInstance.mTinode.addListener(new Tinode.EventListener() {
+                @Override
+                public void onDataMessage(MsgServerData data) {
+                    if (Cache.getTinode().isMe(data.from)) {
+                        return;
+                    }
+                    String webrtc = data.getStringHeader("webrtc");
+                    if (MsgServerData.parseWebRTC(webrtc) != MsgServerData.WebRTC.STARTED) {
+                        return;
+                    }
+                    ComTopic topic = (ComTopic) Cache.getTinode().getTopic(data.topic);
+                    if (topic == null) {
+                        return;
+                    }
+
+                    // Check if we have a later version of the message (which means the call
+                    // has been not yet either accepted or finished).
+                    Storage.Message msg = topic.getMessage(data.seq);
+                    if (msg != null) {
+                        webrtc = msg.getStringHeader("webrtc");
+                        if (webrtc != null && MsgServerData.parseWebRTC(webrtc) != MsgServerData.WebRTC.STARTED) {
+                            return;
+                        }
+                    }
+
+                    CallInProgress call = Cache.getCallInProgress();
+                    if (call == null) {
+                        CallManager.acceptIncomingCall(TindroidApp.getAppContext(),
+                                data.topic, data.seq, data.getBooleanHeader("aonly"));
+                    } else if (!call.equals(data.topic, data.seq)) {
+                        // Another incoming call. Decline.
+                        topic.videoCallHangUp(data.seq);
+                    }
+                }
+
+                @Override
+                public void onInfoMessage(MsgServerInfo info) {
+                    if (MsgServerInfo.parseWhat(info.what) != MsgServerInfo.What.CALL) {
+                        return;
+                    }
+
+                    CallInProgress call = Cache.getCallInProgress();
+                    if (call == null || !call.equals(info.src, info.seq) || !Tinode.TOPIC_ME.equals(info.topic)) {
+                        return;
+                    }
+
+                    // Dismiss call notification.
+                    // Hang-up event received or current user accepted the call from another device.
+                    if (MsgServerInfo.parseEvent(info.event) == MsgServerInfo.Event.HANG_UP ||
+                            (Cache.getTinode().isMe(info.from) &&
+                                    MsgServerInfo.parseEvent(info.event) == MsgServerInfo.Event.ACCEPT)) {
+                        LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(TindroidApp.getAppContext());
+                        final Intent intent = new Intent(TindroidApp.getAppContext(),
+                                HangUpBroadcastReceiver.class);
+                        intent.setAction(Const.INTENT_ACTION_CALL_CLOSE);
+                        intent.putExtra(Const.INTENT_EXTRA_TOPIC, info.src);
+                        intent.putExtra(Const.INTENT_EXTRA_SEQ, info.seq);
+                        lbm.sendBroadcast(intent);
+                    }
+                }
+            });
 
             // Keep in app to prevent garbage collection.
             TindroidApp.retainCache(sInstance);
--- app/src/main/java/co/tinode/tindroid/TindroidApp.java
+++ app/src/main/java/co/tinode/tindroid/TindroidApp.java
@@ -57,12 +57,8 @@ import androidx.work.WorkManager;
 import co.tinode.tindroid.account.ContactsObserver;
 import co.tinode.tindroid.account.Utils;
 import co.tinode.tindroid.db.BaseDb;
-import co.tinode.tinodesdk.ComTopic;
 import co.tinode.tinodesdk.ServerResponseException;
-import co.tinode.tinodesdk.Storage;
 import co.tinode.tinodesdk.Tinode;
-import co.tinode.tinodesdk.model.MsgServerData;
-import co.tinode.tinodesdk.model.MsgServerInfo;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -207,68 +203,6 @@ public class TindroidApp extends Application implements DefaultLifecycleObserver
             editor.putBoolean(Utils.PREFS_USE_TLS, getDefaultTLS());
             editor.apply();
         }
-        // Event handlers for video calls.
-        Cache.getTinode().addListener(new Tinode.EventListener() {
-            @Override
-            public void onDataMessage(MsgServerData data) {
-                if (Cache.getTinode().isMe(data.from)) {
-                    return;
-                }
-                String webrtc = data.getStringHeader("webrtc");
-                if (MsgServerData.parseWebRTC(webrtc) != MsgServerData.WebRTC.STARTED) {
-                    return;
-                }
-                ComTopic topic = (ComTopic) Cache.getTinode().getTopic(data.topic);
-                if (topic == null) {
-                    return;
-                }
-
-                // Check if we have a later version of the message (which means the call
-                // has been not yet either accepted or finished).
-                Storage.Message msg = topic.getMessage(data.seq);
-                if (msg != null) {
-                    webrtc = msg.getStringHeader("webrtc");
-                    if (webrtc != null && MsgServerData.parseWebRTC(webrtc) != MsgServerData.WebRTC.STARTED) {
-                        return;
-                    }
-                }
-
-                CallInProgress call = Cache.getCallInProgress();
-                if (call == null) {
-                    CallManager.acceptIncomingCall(TindroidApp.this,
-                            data.topic, data.seq, data.getBooleanHeader("aonly"));
-                } else if (!call.equals(data.topic, data.seq)) {
-                    // Another incoming call. Decline.
-                    topic.videoCallHangUp(data.seq);
-                }
-            }
-
-            @Override
-            public void onInfoMessage(MsgServerInfo info) {
-                if (MsgServerInfo.parseWhat(info.what) != MsgServerInfo.What.CALL) {
-                    return;
-                }
-
-                CallInProgress call = Cache.getCallInProgress();
-                if (call == null || !call.equals(info.src, info.seq) || !Tinode.TOPIC_ME.equals(info.topic)) {
-                    return;
-                }
-
-                // Dismiss call notification.
-                // Hang-up event received or current user accepted the call from another device.
-                if (MsgServerInfo.parseEvent(info.event) == MsgServerInfo.Event.HANG_UP ||
-                        (Cache.getTinode().isMe(info.from) &&
-                                MsgServerInfo.parseEvent(info.event) == MsgServerInfo.Event.ACCEPT)) {
-                    LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(TindroidApp.this);
-                    final Intent intent = new Intent(TindroidApp.this,
-                            HangUpBroadcastReceiver.class);
-                    intent.setAction(Const.INTENT_ACTION_CALL_CLOSE);
-                    intent.putExtra(Const.INTENT_EXTRA_TOPIC, info.src);
-                    intent.putExtra(Const.INTENT_EXTRA_SEQ, info.seq);
-                    lbm.sendBroadcast(intent);
-                }
-            }
-        });
 
         // Clear completed/failed upload tasks.
         WorkManager.getInstance(this).pruneWork();
